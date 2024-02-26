b2dcb22f30fac63623e1eeb0e1117d19de37ee0e
Michael Variny <michaelvariny@yahoo.com>
2024-02-26 14:48:06 -0500

    testing if whole script works

--- Python_Code/Reporting_Scripts/plotting_to_csv.py
+++ Python_Code/Reporting_Scripts/plotting_to_csv.py
@@ -20,25 +20,25 @@ def pull_data(container_no, sensor):
     match sensor:
         case 'TVOC_Con':
             TVOC_df = collection_df[['Date_Time', 'TVOC_Con']]
-            TVOC_df.to_csv(f'/home/dan/Desktop/CSVs/{time.strftime("%m-%d-%Y")}/TVOC_{container_no}.csv')
+            TVOC_df.to_csv(f'/home/dan/SuperSecretTestFolder/Compost-Monitor/Python_Code/Reporting_Scripts/Test_CSVs/{time.strftime("%m-%d-%Y")}/TVOC_{container_no}.csv')
         case 'CO2_Con':
             CO2_df = collection_df[['Date_Time', 'CO2_Con']]
-            CO2_df.to_csv(f'/home/dan/CompostMonitor_git/Compost-Monitor/PyMongo-Enabled_Scripts/temp_csvs/CO2_{container_no}.csv')
+            CO2_df.to_csv(f'/home/dan/SuperSecretTestFolder/Compost-Monitor/Python_Code/Reporting_Scripts/Test_CSVs/CO2_{container_no}.csv')
         case 'O2_Con':
             O2_df = collection_df[['Date_Time', 'O2_Con']]
-            O2_df.to_csv(f'/home/dan/CompostMonitor_git/Compost-Monitor/PyMongo-Enabled_Scripts/temp_csvs/O2_{container_no}.csv')
+            O2_df.to_csv(f'/home/dan/SuperSecretTestFolder/Compost-Monitor/Python_Code/Reporting_Scripts/Test_CSVs/O2_{container_no}.csv')
         case 'BME_Humidity':
             Hum_df = collection_df[['Date_Time', 'BME_Humidity']]
-            Hum_df.to_csv(f'/home/dan/CompostMonitor_git/Compost-Monitor/PyMongo-Enabled_Scripts/temp_csvs/Humidity_{container_no}.csv')
+            Hum_df.to_csv(f'/home/dan/SuperSecretTestFolder/Compost-Monitor/Python_Code/Reporting_Scripts/Test_CSVs/Humidity_{container_no}.csv')
         case 'BME_Pressure':
             Pressure_df = collection_df[['Date_Time', 'BME_Pressure']]
-            Pressure_df.to_csv(f'/home/dan/CompostMonitor_git/Compost-Monitor/PyMongo-Enabled_Scripts/temp_csvs/Pressure_{container_no}.csv')
+            Pressure_df.to_csv(f'/home/dan/SuperSecretTestFolder/Compost-Monitor/Python_Code/Reporting_Scripts/Test_CSVs/Pressure_{container_no}.csv')
         case 'BME_Temp':
             Temp_df = collection_df[['Date_Time', 'BME_Temp']]
-            Temp_df.to_csv(f'/home/dan/CompostMonitor_git/Compost-Monitor/PyMongo-Enabled_Scripts/temp_csvs/Temp_{container_no}.csv')
+            Temp_df.to_csv(f'/home/dan/SuperSecretTestFolder/Compost-Monitor/Python_Code/Reporting_Scripts/Test_CSVs/Temp_{container_no}.csv')
         case 'Methane_Con':
             Methane_df = collection_df[['Date_Time', 'Methane_Con']]
-            Methane_df.to_csv(f'/home/dan/CompostMonitor_git/Compost-Monitor/PyMongo-Enabled_Scripts/temp_csvs/Methane_{container_no}.csv')            
+            Methane_df.to_csv(f'/home/dan/SuperSecretTestFolder/Compost-Monitor/Python_Code/Reporting_Scripts/Test_CSVs/Methane_{container_no}.csv')            
         case _:
             print(f'{sensor} is not a valid sensor name. Check your sensorNames variable.')
 
@@ -47,14 +47,14 @@ if __name__ == '__main__':
     mainStart = datetime.datetime.now()
     with mp.Manager() as manager:
         
-        sensorNames     =  ['Methane_Con'] #['TVOC_Con', 'CO2_Con', 'O2_Con', 'BME_Humidity', 'BME_Pressure', 'BME_Temp', 'Methane_Con']
+        sensorNames     =  ['TVOC_Con', 'CO2_Con', 'O2_Con', 'BME_Humidity', 'BME_Pressure', 'BME_Temp', 'Methane_Con']
 
         processes = []
 
-        # for container_no in range(1, 2):
-        for sensor_index, sensor in enumerate(sensorNames):
-            process = mp.Process(target = pull_data, args = (1, sensor,))
-            processes.append(process)
+        for container_no in range(1, 2):
+            for sensor_index, sensor in enumerate(sensorNames):
+                process = mp.Process(target = pull_data, args = (1, sensor,))
+                processes.append(process)
         
         for process_index, process in enumerate(processes):
             process.start()

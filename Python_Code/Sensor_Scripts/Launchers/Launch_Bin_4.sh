ec87f964acab7154f47079ed3221f2982594dcad
Czar1249 <danbridavsky@gmail.com>
2024-04-19 17:15:18 -0400

    another minor bugfix - allowing Python scripts to run in parallel using '&' symbol

--- Python_Code/Sensor_Scripts/Launchers/Launch_Bin_4.sh
+++ Python_Code/Sensor_Scripts/Launchers/Launch_Bin_4.sh
@@ -1,7 +1,7 @@
 user=$(whoami)
 data_filepath=/home/"${user}"/CompostingData/
 script_filepath=/home/"${user}"/GitHub_Repositories/Compost-Monitor/Python_Code/Sensor_Scripts
-python3 "${script_filepath}"/EZO_CO2_mongoupload.py  -c /dev/ttyUSB12 -f $data_filepath -n 4 -cn Overall -e April_2024
-python3 "${script_filepath}"/EZO_O2_mongoupload.py   -c /dev/ttyUSB13 -f $data_filepath -n 4 -cn Overall -e April_2024
-python3 "${script_filepath}"/RedBoard_mongoupload.py -c /dev/ttyUSB14 -f $data_filepath -n 4 -cn Overall -e April_2024
-python3 "${script_filepath}"/methane_mongoupload.py  -c /dev/ttyUSB15 -f $data_filepath -n 4 -cn Overall -e April_2024
+python3 "${script_filepath}"/EZO_CO2_mongoupload.py  -c /dev/ttyUSB12 -f $data_filepath -n 4 -cn Overall -e April_2024 &
+python3 "${script_filepath}"/EZO_O2_mongoupload.py   -c /dev/ttyUSB13 -f $data_filepath -n 4 -cn Overall -e April_2024 &
+python3 "${script_filepath}"/RedBoard_mongoupload.py -c /dev/ttyUSB14 -f $data_filepath -n 4 -cn Overall -e April_2024 &
+python3 "${script_filepath}"/methane_mongoupload.py  -c /dev/ttyUSB15 -f $data_filepath -n 4 -cn Overall -e April_2024 &

d259cf5cc1a420a559266ea76f14586860f72681
Czar1249 <danbridavsky@gmail.com>
2024-04-19 16:37:43 -0400

    Quick fix - changed script filepath in python call

--- Python_Code/Sensor_Scripts/Launchers/Launch_Bin_3.sh
+++ Python_Code/Sensor_Scripts/Launchers/Launch_Bin_3.sh
@@ -1,6 +1,7 @@
 user=$(whoami)
-filepath=/home/"${user}"/CompostingData/
-python3 EZO_CO2_mongoupload.py  -c /dev/ttyUSB8  -f $filepath -n 3 -cn Overall -e April_2024
-python3 EZO_O2_mongoupload.py   -c /dev/ttyUSB9  -f $filepath -n 3 -cn Overall -e April_2024
-python3 RedBoard_mongoupload.py -c /dev/ttyUSB10 -f $filepath -n 3 -cn Overall -e April_2024
-python3 methane_mongoupload.py  -c /dev/ttyUSB11 -f $filepath -n 3 -cn Overall -e April_2024
+data_filepath=/home/"${user}"/CompostingData/
+script_filepath=/home/"${user}"/GitHub_Repositories/Compost-Monitor/Python_Code/Sensor_Scripts
+python3 "${script_filepath}"/EZO_CO2_mongoupload.py  -c /dev/ttyUSB8  -f $filepath -n 3 -cn Overall -e April_2024
+python3 "${script_filepath}"/EZO_O2_mongoupload.py   -c /dev/ttyUSB9  -f $filepath -n 3 -cn Overall -e April_2024
+python3 "${script_filepath}"/RedBoard_mongoupload.py -c /dev/ttyUSB10 -f $filepath -n 3 -cn Overall -e April_2024
+python3 "${script_filepath}"/methane_mongoupload.py  -c /dev/ttyUSB11 -f $filepath -n 3 -cn Overall -e April_2024

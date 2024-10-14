845cb7131940c87201429b98f035397662e273fe
Michael Variny <michaelvariny@yahoo.com>
2024-10-14 16:22:38 -0400

    changed filepath to correct

--- Python_Code/Sensor_Scripts/Launchers/Launch_Bin_3.sh
+++ Python_Code/Sensor_Scripts/Launchers/Launch_Bin_3.sh
@@ -1,6 +1,6 @@
 user=$(whoami)
 data_filepath=/home/"${user}"/CompostingData/
-script_filepath=/home/"${user}"/GitHub_Repositories/Compost-Monitor/Python_Code/Sensor_Scripts
+script_filepath=/home/"${user}"/GitHub_Repositories/Compost-Monitor/Python_Code/SQL
 python3 "${script_filepath}"/CO2_sqlUpload.py  -c /dev/ttyUSB0  -f $data_filepath -n 3 -cn Jun28Experiment -e June_2024 &
 python3 "${script_filepath}"/O2_sqlUpload.py   -c /dev/ttyUSB1  -f $data_filepath -n 3 -cn Jun28Experiment -e June_2024 &
 python3 "${script_filepath}"/RedBoard_mongoupload.py -c /dev/ttyUSB2 -f $data_filepath -n 3 -cn Jun28Experiment -e June_2024 &

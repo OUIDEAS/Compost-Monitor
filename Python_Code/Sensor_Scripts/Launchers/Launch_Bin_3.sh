f3578e019f38165b6036849fe928bf00b15b98c5
Michael Variny <michaelvariny@yahoo.com>
2024-10-15 13:40:04 -0400

    updated bash script

--- Python_Code/Sensor_Scripts/Launchers/Launch_Bin_3.sh
+++ Python_Code/Sensor_Scripts/Launchers/Launch_Bin_3.sh
@@ -4,5 +4,5 @@ script_filepath=/home/"${user}"/GitHub_Repositories/Compost-Monitor/Python_Code/
 script_filepath2=/home/"${user}"/GitHub_Repositories/Compost-Monitor/Python_Code/SQL
 python3 "${script_filepath2}"/CO2_sqlUpload.py  -c /dev/ttyUSB0  -f $data_filepath -n 3 &
 python3 "${script_filepath2}"/O2_sqlUpload.py   -c /dev/ttyUSB1  -f $data_filepath -n 3 &
-python3 "${script_filepath}"/RedBoard_mongoupload.py -c /dev/ttyUSB2 -f $data_filepath -n 3 -cn Jun28Experiment -e June_2024 &
+python3 "${script_filepath2}"/RedBoard_mongoupload.py -c /dev/ttyUSB2 -f $data_filepath -n 3 &
 python3 "${script_filepath}"/methane_mongoupload.py  -c /dev/ttyUSB3 -f $data_filepath -n 3 -cn Jun28Experiment -e June_2024 &

c89ab217c2776b51b362f052be2f80f20bea3dd3
Michael Variny <michaelvariny@yahoo.com>
2024-10-16 13:11:35 -0400

    changed USB

--- Python_Code/Sensor_Scripts/Launchers/Launch_Bin_3.sh
+++ Python_Code/Sensor_Scripts/Launchers/Launch_Bin_3.sh
@@ -2,7 +2,7 @@ user=$(whoami)
 data_filepath=/home/"${user}"/CompostingData/
 script_filepath=/home/"${user}"/GitHub_Repositories/Compost-Monitor/Python_Code/Sensor_Scripts
 script_filepath2=/home/"${user}"/GitHub_Repositories/Compost-Monitor/Python_Code/SQL
-python3 "${script_filepath2}"/CO2_sqlUpload.py  -c /dev/ttyUSB0  -f $data_filepath -n 3 &
+python3 "${script_filepath2}"/CO2_sqlUpload.py  -c /dev/CO2B3  -f $data_filepath -n 3 &
 python3 "${script_filepath2}"/O2_sqlUpload.py   -c /dev/ttyUSB1  -f $data_filepath -n 3 &
 python3 "${script_filepath2}"/RedBoard_sqlUpload.py -c /dev/ttyUSB2 -f $data_filepath -n 3 &
 python3 "${script_filepath2}"/CH4_sqlUpload.py  -c /dev/ttyUSB3 -f $data_filepath -n 3 &
